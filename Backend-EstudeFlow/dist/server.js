f456e37b01d6e422df95512a03450be8bd96f21b
Ingryd <ingrydcosta@live.com>
2025-06-26 11:31:42 -0300

    :sparkles: Backend finished

--- Backend-EstudeFlow/dist/server.js
+++ Backend-EstudeFlow/dist/server.js
@@ -20,7 +20,7 @@ console.log('Variáveis de ambiente:', {
     DATABASE_URL: process.env.DATABASE_URL,
 });
 const corsOptions = {
-    origin: 'http://localhost:5175',
+    origin: 'http://localhost:5173',
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
--- Backend-EstudeFlow/src/app.ts
+++ Backend-EstudeFlow/src/app.ts
@@ -7,7 +7,7 @@ import { errorHandler } from './middlewares/errorHandler';
 const app = express();
 
 const corsOptions = {
-  origin: '*',
+  origin: 'http://localhost:5173',
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
